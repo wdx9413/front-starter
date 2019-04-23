20489f5b3678fcc61e878b4815aedb89bc0567cb
wdx9413@163.com <wdx9413@163.com>
2019-04-23 13:40:21 +0800

    feature: react-redux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from "./component/App.js";
+import {Provider} from 'react-redux'
+import store from './store'
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById("root")
 );
---
+++ src/store/index.js
@@ -0,0 +1,3 @@
+import {combineReducers, createStore} from 'redux'
+
+export default createStore(combineReducers({}));
